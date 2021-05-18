7cf5ba4d8292ae521ededc59f985e67ab8096e95
Teosoph <teosoph@hotmail.com>
2021-05-18 17:48:42 +0300

    dockers

---
+++ lesson3/test.py
@@ -0,0 +1,12 @@
+from time import sleep
+
+def main():
+    print("Hello my friend!!!")
+    for i in range(100):
+        print(i)
+        sleep(1)
+    #number = input("number: ")
+    print(f"You viewed numbers. Thank You!!!")
+
+if __name__ == "__main__":
+    main()
